8bcba33505b88c35a0776f9c38e05034abf4b208
owdaa <awadodeh@gmail.com>
2017-10-13 12:36:02 -0500

    Use Qeurydsl.

---
+++ src/main/java/com/example/university/repo/StudentExpressions.java
@@ -0,0 +1,20 @@
+package com.example.university.repo;
+
+import sun.tools.tree.BooleanExpression;
+
+/**
+ * Check this out.
+ * Querydsl, which a dynamic search criteria
+ */
+public class StudentExpressions {
+
+//    public static BooleanExpression hasLastName( String lastName){
+//        return QStudent.student.attendee.lastName.eq(lastName);
+//    }
+
+}
+
+/**
+ * use case as follows:
+ * studentRepository.findAll(hasLastName("awad").and(isFullTime()).and(isOlderThan(20));
+ * */
